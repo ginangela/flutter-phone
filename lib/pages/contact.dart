14d8fb3ce4b4d2d52d8ceec873cc50902bdf8aaf
NazeeraAlthea <ryamaulana2894@gmail.com>
2025-05-04 16:34:55 +0700

    folder sturcture

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_phone/add_contact.dart';
+import 'package:flutter_phone/pages/add_contact.dart';
 
 void main() {
   runApp(const ContactsApp());
